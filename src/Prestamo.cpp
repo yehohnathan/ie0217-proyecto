99407ac40c0407aff2a3194c4dc0c77f555bae58
Danny Solórzano <dannysolorzano71@gmail.com>
2024-02-03 19:29:19 -0600

    Se agregan los métodos para pagar una cuota y para generar el reporte de cuotas
    Signed-off-by: Danny Solórzano <dannysolorzano71@gmail.com>

--- src/Prestamo.cpp
+++ src/Prestamo.cpp
@@ -1,37 +1,38 @@
 #include "Prestamo.hpp"
 
+//< Método para establecer los datos del préstamo
 void Prestamo::setDatos(string tipoMoneda_p, double montoTotal_p, int cuotasTotal_p, double tasaInteres_p){
-    tipoMoneda = tipoMoneda_p;       //< Inicializa el atributo tipoMoneda
-    montoTotal = montoTotal_p;       //< Inicializa el atributo montoTotal
-    cuotasTotal = cuotasTotal_p;     //< Inicializa el atributo cuotasTatales
-    tasaInteres = tasaInteres_p;     //< Inicializa el atributo tasaInteres
+    tipoMoneda = tipoMoneda_p;       //< Se inicializa el atributo tipoMoneda
+    montoTotal = montoTotal_p;       //< Se inicializa el atributo montoTotal
+    cuotasTotal = cuotasTotal_p;     //< Se inicializa el atributo cuotasTotal
+    tasaInteres = tasaInteres_p;     //< Se inicializa el atributo tasaInteres
 
-    /* Calcula el valor de una cuota*/
+    //< Calcula el costo de la cuota
     costoCuota = (montoTotal*tasaInteres*pow(1+tasaInteres,cuotasTotal))/(pow(1+tasaInteres,cuotasTotal) - 1);
-    cuotasPagadas = 0;      //< Cantidad de cuotas pagadas
-    interesesPagados = 0;   //< Cantidad de intereses pagados
-    deudaPagada = 0;        //< Parte de la deuda pagada
+    cuotasPagadas = 0;      //< La cantidad de cuotas pagadas es inicialmente cero
+    interesesPagados = 0;   //< La cantidad de intereses pagados es inicialmente cero
+    deudaPagada = 0;        //< La parte de la deuda pagada es inicialmente cero
     
-    double montoRestante = montoTotal;      //< 
+    double montoRestante = montoTotal;      //< El monto restante es inicialmente el monto total de la deuda
     for(int i = 1; i <= cuotasTotal; i++){
-        /* Calcula el aporte a intereses */
+        //< Se calcula el aporte de intereses
         double aporteIntereses = montoRestante*tasaInteres;
-        /* Calcula el aporte a la deuda*/
+        //< Se calcula el aporte a la deuda
         double aporteDeuda = costoCuota - aporteIntereses;
-        /* Actualiza el monto restante del prestamo*/
+        //< Se actualiza el monto restante del préstamo
         montoRestante -= aporteDeuda;
-        /* Verifica si el monto es menor a cero y en caso de serlo lo ajusta a cero*/
+        //< Se verifica si el monto es menor a cero y en caso de serlo lo ajusta a cero
         if(montoRestante < 0){
             montoRestante = 0;      //< Utilizado para evitar valores negativos
         }
-        /* Define un string con la informacion de la cuota*/
+        //< Se define un string con la información de la cuota
         string infoCuota = "Cuota " + to_string(i) + ", " + to_string(costoCuota) + ", " + to_string(aporteDeuda) + ", " + to_string(aporteIntereses) + ", " + to_string(montoRestante);
-        /* La agrega al contenedor */
+        // Se agrega la información de la cuota al contenedor con la información de las cuotas
         infoCuotas.push_back(infoCuota);
     }
 }
 
-/* Función que imprime toda la información relacionada al préstamo*/
+//< Método que imprime toda la información relacionada al préstamo
 void Prestamo::imprimirInfo(){
     cout << "\n---Prestamo numero " << numeroPrestamo << "---" << endl;
     cout << "Monto total: " << montoTotal << endl;
@@ -39,4 +40,42 @@ void Prestamo::imprimirInfo(){
     cout << "Tasa de interes: " << tasaInteres << endl;
     cout << "Costo de la cuota: " << costoCuota << endl;
     cout << endl;
+}
+
+//< Método para pagar una cuota
+void Prestamo::pagarCuota(){
+    //< Se suma 1 a la cantidad de cuotas pagadas
+    cuotasPagadas += 1;
+    //< Se calcula el monto restante antes de pagar la cuota
+    double montoRestante = montoTotal - deudaPagada;
+    //< Se calcula el aporte de intereses de la cuota a pagar
+    double aporteIntereses = montoRestante*tasaInteres;
+    //< Se calcula el aporte de la deuda de la cuota a pagar
+    double aporteDeuda = costoCuota - aporteIntereses;
+    //< Se agrega el aporte de intereses a la cantidad de interes pagados
+    interesesPagados += aporteIntereses;
+    //< Se agrega el aporte de la deuda a la cantidad de deuda pagada
+    deudaPagada += aporteDeuda;
+}
+
+//< Método para generar el reporte de cuotas
+vector <string> Prestamo::generarReporteCuotas(){
+    // Se declara una vector de strings que contendrá la información del reporte
+    vector <string> reporte;
+    // Se agrega un encabezado para cada columna del reporte
+    reporte.push_back("numero de cuota, costo de cuota, aporte deuda, aporte intereses, monto restante");
+    // Se agrega un encabezado para las cuotas pagadas
+    reporte.push_back("Cuotas pagadas");
+    // Se agrega la información de cada una de las cuotas pagadas al reporte
+    for(int i = 0; i < cuotasPagadas; i++){
+        reporte.push_back(infoCuotas[i]);
+    }
+    // Se agrega un encabezado para las cuotas restantes
+    reporte.push_back("Cuotas restantes");
+    // Se agrega la información de cada una de las cuotas restantes al reporte
+    for(int i = cuotasPagadas; i < cuotasTotal; i++){
+        reporte.push_back(infoCuotas[i]);
+    }
+    // Se retorna el vector de strings que incluye la información del reporte
+    return reporte;
 }
--- src/Prestamo.hpp
+++ src/Prestamo.hpp
@@ -1,10 +1,10 @@
  /**
  * @file Prestamo.hpp
- * @author David Madrigal, Danny Solozarlo, Yehohnathan Miranda
+ * @author David Madrigal, Danny Solórzano, Yehohnathan Miranda
  * @version 0.1
- * @date 2024-03-2
+ * @date 2024-02-03
  * @copyright Copyright (c) 2024
- * @brief Archivo que clos atributos y metodos de la clase Prestamo.
+ * @brief Archivo que incluye los atributos y métodos de la clase Prestamo.
  */
 
 #ifndef PRESTAMO_HPP
@@ -13,45 +13,48 @@
 #include "LecturaDatos.hpp"
 
 /**
-* @brief Clase estable los datos del prestamo.
+* @brief Clase que permite gestionar los préstamos de los usuarios.
 * 
-* La clase Prestamo tiene como métodos y atributos toda la información.
-* relacionada con un prestamo como el monto, cuotas, tasa de interés... etc.
-* Además, tiene la capacidar de guardar la información sobre la cuotas
-* de la deuda.
+* La clase Prestamo tiene como métodos y atributos toda la información
+* relacionada con un préstamo, como el monto, cuotas, tasa de interés, etc.
+* Además, guarda la información sobre las cuotas de la deuda.
 */
 class Prestamo {
     public:
-        int numeroPrestamo;         //< Numero del Prestamo
-        string tipoMoneda;          //< Tipo de moneada a usar
-        double montoTotal;          //< Monto de la deuda
-        int cuotasTotal;            //< Cuotas de la deuda
-        double tasaInteres;         //< Tasa de interes
+        int numeroPrestamo;         //< Número del préstamo
+        string tipoMoneda;          //< Tipo de moneda en que se solicitó el prestamo
+        double montoTotal;          //< Monto total de la deuda
+        int cuotasTotal;            //< Cantidad total de cuotas de la deuda
+        double tasaInteres;         //< Tasa de interés
         double costoCuota;          //< Costo de la cuota
-        int cuotasPagadas;          //< Cantidad de Cuotas pagadas
+        int cuotasPagadas;          //< Cantidad de cuotas pagadas
         double interesesPagados;    //< Intereses pagados
         double deudaPagada;         //< Deuda pagada
-        vector <string> infoCuotas; //< Contendor con información de la cuotas
+        vector <string> infoCuotas; //< Contendor con toda la información de las cuotas
 
         /**
-        * @brief Establece los datos de la clase.
+        * @brief Establece los datos de un préstamo.
         *
         * @param tipoMoneda_p Tipo de moneda del préstamo.
         * @param montoTotal_p Monto total del préstamo.
-        * @param cuotasTotal_p Cuotas del préstamo.
+        * @param cuotasTotal_p Cantidad total de cuotas del préstamo.
         * @param tasaInteres_p Tasa de interés del préstamo.
         */
         void setDatos(string tipoMoneda_p, double montoTotal_p, int cuotasTotal_p, double tasaInteres_p);
 
         /**
-         * @brief Imprime información del préstamo
+         * @brief Imprime la información relevante del préstamo.
          */
         void imprimirInfo();
+
         /**
-         * @brief Paga la cuota de un préstamo
+         * @brief Paga una cuota de un préstamo.
          * 
          */
         void pagarCuota();
+        /**
+         * @brief Genera un reporte con toda la información sobre las cuotas del préstamo.
+        */
         vector <string> generarReporteCuotas();
 };
 
