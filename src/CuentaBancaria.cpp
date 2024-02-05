2ecebd94e6d2acc33a59f1d210583d6f0a69309b
Danny Solórzano <dannysolorzano71@gmail.com>
2024-02-04 22:04:52 -0600

    Se agregan los métodos para retirar, depositar y transferir dinero entre las cuentas bancarias
    Signed-off-by: Danny Solórzano <dannysolorzano71@gmail.com>

--- src/CuentaBancaria.cpp
+++ src/CuentaBancaria.cpp
@@ -17,3 +17,53 @@ void CuentaBancaria::imprimirInfo(){
     cout << "Tipo de moneda: " << tipoMoneda << endl;
     cout << "Dinero de ahorros: " << dineroAhorros << endl;
 }
+
+// Este método permite depositar dinero a una cuenta
+void CuentaBancaria::depositar(){
+    // Se define una variable que guardará el monto a depositar
+    double montoDepositar;
+    // Se recibe del usuario el monto a depositar
+    cout << "Ingrese el monto que desea depositar: ";
+    cin >> montoDepositar;
+    // Se agrega esa cantidad al dinero de ahorros de la cuenta
+    dineroAhorros += montoDepositar;
+}
+
+// Este método permite retirar dinero de una cuenta
+void CuentaBancaria::retirar(){
+    // Se define una variable que guardará el monto a retirar
+    double montoRetirar;
+    // Se recibe del usuario el monto a retirar
+    cout << "Ingrese el monto que desea retirar: ";
+    cin >> montoRetirar;
+    // Se revisa si el monto a retirar es mayor que el dinero que hay en la cuenta
+    if(montoRetirar > dineroAhorros){
+        // En caso de que sea mayor se imprime un mensaje que indica que no hay fondos suficientes
+        cout << "No hay suficiente dinero en la cuenta." << endl;
+        // Se termina la ejecución del método
+        return;
+    }
+    // Si todo sale bien se retira esa cantidad del dinero de ahorros de la cuenta
+    dineroAhorros -= montoRetirar;
+}
+
+// Este método permite transferir dinero de una cuenta a otra
+// Se recibe una referencia a otro objeto de la clase CuentaBancaria que es la cuenta destino
+void CuentaBancaria::transferir(CuentaBancaria& cuentaDestino){
+    // Se define una variable que guardará el monto a transferir
+    double montoTransferir;
+    // Se recibe del usuario el monto a transferir
+    cout << "Ingrese el monto que desea transferir: ";
+    cin >> montoTransferir;
+    // Se revisa si el monto a transferir es mayor que el dinero que hay en la cuenta de salida
+    if(montoTransferir > dineroAhorros){
+        // En caso de que sea mayor se imprime un mensaje que indica que no hay fondos suficientes
+        cout << "No hay suficiente dinero en la cuenta de salida." << endl;
+        // Se termina la ejecución del método
+        return;
+    }
+    // Si todo sale bien se retira esa cantidad del dinero de ahorros de la cuenta de salida
+    dineroAhorros -= montoTransferir;
+    // Se agrega esa cantidad al dinero de ahorros de la cuenta destino
+    cuentaDestino.dineroAhorros += montoTransferir;
+}
