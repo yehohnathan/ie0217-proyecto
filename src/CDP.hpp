fd9f47f6404e1c5fdb6ea8ef47e708f73df8f41a
Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>
2024-02-05 18:17:26 -0600

    Comentarios del header CDP
    Signed-off-by: Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>

--- src/CDP.hpp
+++ src/CDP.hpp
@@ -1,10 +1,10 @@
  /**
- * @file CuentaBancaria.hpp
+ * @file CDP.hpp
  * @author David Madrigal, Danny Solórzano, Yehohnathan Miranda
  * @version 0.1
  * @date 2024-02-04
  * @copyright Copyright (c) 2024
- * @brief Archivo que incluye los atributos y métodos de la clase CuentaBancaria.
+ * @brief Contiene todo lo relacionado a los certificados a plazo (CDP). 
  */
 
 #ifndef CDP_HPP
@@ -15,18 +15,38 @@
 #include <string>
 using namespace std;
 
+/**
+* @brief Clase que permite gestionar los CDP del usuario.
+* 
+* La clase CDP tiene como métodos y atributos todo lo relacionado a CDP
+* para que el cliente pueda solicitar CDP con los intereses y tiempos
+* deseados.
+*/
 class CDP {
     public:
-        string tipoMoneda;
-        double dineroCDP;
-        int fechaCreacion[3];
-        int fechaExpira[3];
-        int plazoMeses;
-        double tasaInteres;
-        string estado;
-        string informacion;
-
+        string tipoMoneda;      //< Tipo de moneda (dolares o colones) del CDP
+        double dineroCDP;       //< Dinero (cuota) mensual que debe ingresar el cliente a su CDP
+        int fechaCreacion[3];   //< Esta es la fecha que se introduce al inicio del programa, es la fecha de inicio.
+        int fechaExpira[3];     //< Fecha de expiración del CDP, depende si es de pequeño, mediado o largo plazo
+        int plazoMeses;         //< Define si el plazo es corto, mediado o largo  
+        double tasaInteres;     //< Depende del plazo de intereses, 
+        string estado;          //< Define si el CDP esta vigente o ya se vencio
+        string informacion;     //< Contiene la información en string de los atributos mostrados anteriormente menos tipoMoneda, dineroCDP
+        
+        /**
+         * @brief Establece las características del CDP como tipo de moneda, plazos, intereses y dinero.
+         */
         void setDatos(int fechaCreacion_p[], string tipoMoneda_P, int plazoMeses_p, double dineroCDP, double tasaInteres);
+
+        /**
+         * @brief Muestra datos simples como dineroCDP, tipo de moneda y taza de intereses.
+         */
+        void mostrarDatosCDP();
+
+        /**
+         * @brief Actualiza la linea temporal del CDP para saber si ya llegó a su vencimiento //< Esto esta a prueba
+         */
+        void actualizarCDP(int fecha[]);
 };
 
 #endif // CDP_HPP
