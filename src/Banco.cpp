7ede10fc70d15f2f331e143819c8d03a1cc7b014
Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>
2024-02-08 23:56:14 -0600

    Adición de método de Atención y Fecha
    Signed-off-by: Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>

--- src/Banco.cpp
+++ src/Banco.cpp
@@ -75,3 +75,58 @@ void Banco::mostrarMenu(){
         }
     }
 }
+
+
+// Se ejecuta el método que permite la gestión de procesos mediante el objeto ventanilla
+void Banco::atenderCliente(){
+    // se instancia un variable que almacena el id del cliente
+    int idCliente;
+    cout << "Ingrese su id: ";
+    // El usuario ingresa su id de cliente
+    cin >> idCliente;
+    auto it_cliente = clientes.find(idCliente); // Luego se pregunta si dentro del map hay un cliente con ese info de id
+    if(it_cliente == clientes.end()){   // Si se llegó al final del map, significa que el id ingresado no pertenece a los clientes
+        cout << "El id ingresado no existe en el registro." << endl;
+        return; // Retorna la función, y hace regrese al menú
+    }
+    // Como el id si existió, entonces se utiliza el método mostrarMenu del objeto ventanilla.
+    // En este se ingresa la info del cliente que ingreso el id, la info de todos los clientes, la fecha
+    // y el vector de transacciones para que guarde la info de la transancción que se realizó
+    ventanilla.mostrarMenu(fecha, it_cliente->second, clientes, transacciones);
+}
+
+// Método que debe ejecutarse al inicio del programa para garantizar el proceso del banco
+void Banco::setFecha(){
+    // Se crea un ciclo while que se repite hasta que se ingrese una fecha válida
+    while(1){
+        // Se le solicita la fecha al usuario
+        cout << "Ingrese la fecha de hoy (dia mes año): ";
+        try{
+            // La fecha será guarda en un vector de tamaño 3. Se debería ingresar XX YY ZZZZ
+            for(int i = 0; i < 3; i++){
+                // Se van ingresando los datos al array fecha
+                cin >> fecha[i];
+            }
+            // Luego se pregunta si se ingresó algo que no fuese un número entero
+            if(cin.fail()){
+                // Se limpia cin
+                cin.clear();
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                // Lanza un error para señalar que lo que se ingreso no fue correcto 
+                throw invalid_argument("Todas las entradas deben ser numeros.");
+            }
+            // Se pregunta si lo ingresado esta en el formato correcto
+            if(fecha[0] <= 0 || fecha[0] > 31 || fecha[1] <= 0 || fecha[1] > 12){
+                throw "La fecha ingresada no es valida.";
+            }
+            break;
+        }
+        // Muestra el mensaje de error lanzado
+        catch(const exception& e){
+            cerr << e.what() << endl;
+        }      
+        catch(const char* mensajeError){
+            cerr << mensajeError << endl;
+        }    
+    }
+}
