e8a482aa0c0006d99672c656db09e74dd5b73a43
Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>
2024-02-10 17:02:03 -0600

    Adición y eliminación de atributos/métodos
    Se añadieron atributos de identificación y se eliminó el vector llamado información, ya que se consideró que no era necesario mostrar una tabla con la información de cada cuota ingresada por el cliente al CDP. Además se eliminó el método mostrarInfo porque no aportaba mucho y se reincertó el método actualizarCDP
    
    Signed-off-by: Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>

--- src/CDP.hpp
+++ src/CDP.hpp
@@ -15,6 +15,9 @@
 #include <string>
 using namespace std;
 
+// Cuando la clase que permite leer datos y verificar que cumple con lo solicitado, este lista, se instanciará
+// #include "LecturaDatos.hpp"
+
 /**
 * @brief Clase que permite gestionar los CDP del usuario.
 * 
@@ -24,37 +27,39 @@ using namespace std;
 */
 class CDP {
     public:
+        int numeroCDP;          //< Identificación del CDP
+        int idPropietario;      //< Identificación del propietario
+        string tipo;            //< Especifica el tipo de CDP que solicito el cliente
         string tipoMoneda;      //< Tipo de moneda (dolares o colones) del CDP
         double dineroCDP;       //< Dinero (suma de cuotas) que tiene el cliente en su CDP
+
         int fechaCreacion[3];   //< Esta es la fecha que se introduce al inicio del programa, es la fecha de inicio.
         int fechaExpira[3];     //< Fecha de expiración del CDP, depende si es de pequeño, mediado o largo plazo
+        
         int plazoMeses;         //< Define si el plazo es corto, mediado o largo  
         double tasaInteres;     //< Depende del plazo de intereses, 
         string estado;          //< Define si el CDP esta vigente o ya se vencio
-        string informacion;     //< Contiene la información en string de los atributos mostrados anteriormente menos tipoMoneda, dineroCDP
-        
+
         /**
          * @brief Establece las características del CDP como tipo de moneda, plazos, intereses y dinero.
          * 
-         * @param fechaCreacion_p   Fecha de inicio del CDP
-         * @param tipoMoneda_P      Moneda del CDP (dolares y colones)  
-         * @param plazoMeses_p      Plazo de meses del CDP (corto, mediano y largo)
-         * @param dineroCDP         Dinero del CDP
-         * @param tasaInteres       Tasa de interes del CDP
+         * @param idPropietario_p Identificación del cliente dueño del CDP
+         * @param numeroCDP_p Indentificador del CDP
+         * @param tipoMoneda_P Moneda del CDP (dolares y colones)  
+         * @param dineroCDP_p Dinero del CDP
+         * @param fechaCreacion_p Fecha de inicio del CDP
+         * @param plazoMeses_p Plazo de meses del CDP (corto, mediano y largo)
+         * @param tasaInteres_p Tasa de interes del CDP
+         * @param tipo Tipo de CDP solicitado
          */
-        void setDatos(int fechaCreacion_p[], string tipoMoneda_P, int plazoMeses_p, double dineroCDP, double tasaInteres);
-
-        /**
-         * @brief Muestra datos simples como dineroCDP, tipo de moneda y taza de intereses.
-         */
-        void mostrarDatosCDP();
+        void setDatos(int idPropietario_p, int numeroCDP_p, string tipoMoneda_p, double dineroCDP_p, int fechaCreacion_p[], int plazoMeses_p, double tasaInteres_p, string tipo_p);
 
         /**
          * @brief Actualiza la linea temporal del CDP para saber si ya llegó a su vencimiento
          * 
-         * @param fecha     Atributo para calcular la fecha actual del CDP
+         * @param fecha Atributo para calcular la fecha actual del CDP
          */
-        // void actualizarCDP(int fecha[]); //< Esto esta a prueba
+        void actualizarCDP(int fecha[]);
 };
 
 #endif // CDP_HPP
