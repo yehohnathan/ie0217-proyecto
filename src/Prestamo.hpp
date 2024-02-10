800ac6e645db0b9d5b0b2e6b3ac9dd82b130e142
Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>
2024-02-10 15:50:31 -0600

    Nuevos atributos y modificación en métodos
    Se añadieron dos métodos nuevos, que permiten darle una identificación a los prestamos y otro que permite saber el tipo de prestamo que se solicitó por el cliente. Estos nuevos métodos fueron ingresados en el método setDatos, además de modificar un poco el estilo de los comentarios.
    
    Signed-off-by: Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>

--- src/Prestamo.hpp
+++ src/Prestamo.hpp
@@ -16,6 +16,10 @@
 #include <math.h>
 using namespace std;
 
+// Librería fundamental que permite la verificación de todos los datos ingresados por el usuario
+// Se espera el git push que la instancie por Danny
+// #include "LecturaDatos.hpp"
+
 /**
 * @brief Clase que permite gestionar los préstamos de los usuarios.
 * 
@@ -25,27 +29,35 @@ using namespace std;
 */
 class Prestamo {
     public:
-        int numeroPrestamo;         //< Número del préstamo
+        int numeroPrestamo;         //< Número del préstamo, el cliente puede solicitar nuevos
+        int idPropietario;          //< Identificador del prestamo según el cliente
+        string tipo;                //< Contiene el tipo de prestamo que es el objeto
         string tipoMoneda;          //< Tipo de moneda en que se solicitó el prestamo
         double montoTotal;          //< Monto total de la deuda
+
         int cuotasTotal;            //< Cantidad total de cuotas de la deuda
         double tasaInteres;         //< Tasa de interés
         double costoCuota;          //< Costo de la cuota
+
         int cuotasPagadas;          //< Cantidad de cuotas pagadas
         double interesesPagados;    //< Intereses pagados
         double deudaPagada;         //< Deuda pagada
+
         vector <string> infoCuotas; //< Contendor con toda la información de las cuotas
 
         /**
         * @brief Establece los datos de un préstamo.
         *
+        * @param idPropietario_p Corresponde al id del cliente
+        * @param numeroPrestamo_p Numero de creación del prestamo según el cliente
         * @param tipoMoneda_p Tipo de moneda del préstamo.
         * @param montoTotal_p Monto total del préstamo.
         * @param cuotasTotal_p Cantidad total de cuotas del préstamo.
         * @param tasaInteres_p Tasa de interés del préstamo.
+        * @param cuotasPagadas_p Total de cuotas pagadas
+        * @param tipo_p Tipo de prestamo que sacó el cliente
         */
-        void setDatos(string tipoMoneda_p, double montoTotal_p, int cuotasTotal_p, double tasaInteres_p);
-
+        void setDatos(int idPropietario_p, int numeroPrestamo_p, string tipoMoneda_p, double montoTotal_p, int cuotasTotal_p, double tasaInteres_p, int cuotasPagadas_p, string tipo_p);
         /**
          * @brief Imprime la información relevante del préstamo.
          */
@@ -59,7 +71,7 @@ class Prestamo {
         /**
          * @brief Genera un reporte con toda la información sobre las cuotas del préstamo.
         */
-        vector <string> generarReporteCuotas();
+        vector <string> generarInfoCuotas();
 };
 
 #endif // PRESTAMO_HPP
