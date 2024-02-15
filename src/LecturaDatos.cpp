7085123c0092eaacf9959b8e0e00d800226cb91c
Danny Solorzano Mayorga <dannysolorzano71@gmail.com>
2024-02-14 18:46:33 -0600

    Implementacion de una funcion para leer numeros enteros con manejo de excepciones, para leer opciones y cantidad de cuotas, etc
    Signed-off-by: Danny Solorzano Mayorga <dannysolorzano71@gmail.com>

--- src/LecturaDatos.cpp
+++ src/LecturaDatos.cpp
@@ -39,3 +39,50 @@ void leerDouble(double& variableDouble, string mensaje){
     // Cuando todo esta bien, se asigna el valor de la lectura a la variable original. 
     variableDouble = lectura;
 }
+
+// Funcion para leer un número entero del usuario. 
+void leerEntero(int& variableEntero, string mensaje){
+    // Se crea una variable para realizar la lectura. 
+    double lectura;
+    while(1){
+        try{
+            // Se intenta leer la entrada del usuario. 
+            cout << mensaje;
+            cin >> lectura;
+            // Si la lectura falla es porque no se ingresó un número. 
+            if(cin.fail()){
+                // Se ignora la entrada ingresada por el usuario. 
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                // Se lanza una excepción con un mensaje que indica que se debe ingresar un número. 
+                throw invalid_argument("Debe ingresar un numero. Intente de nuevo.");
+            }
+            // Si el número ingresado no es un número entero o no es un número positivo. 
+            if(lectura != floor(lectura) || lectura <= 0){
+                // Se lanza una excepción con un mensaje que indica que se debe ingresar un entero positivo. 
+                throw "Debe ingresar un entero positivo. Intente de nuevo.";
+            }
+            // Se produce un string con la lectura realizada. 
+            string string_lectura = to_string(int(lectura));
+            // Si la cantidad de dígitos es mayor a 9.
+            if(string_lectura.size() > 9){
+                // Se lanza una excepcion que indica que el número debe tener a lo sumo 9 dígitos. 
+                throw "El numero debe ser menor a 1000000000. Intente de nuevo.";
+            }
+            // Si todo sale bien se puede salir del bucle. Si no continuará leyendo entradas hasta ingresar una entrada válida. 
+            break;
+        } 
+        // Se recibe la excepción si la entrada ingresada no es un número. 
+        catch(const exception& e){
+            // Se imprime el mensaje de error de la excepción. 
+            cerr << e.what() << endl;
+        }
+        // Se recibe la excepción si el número ingresado no es un entero positivo. 
+        catch(const char* mensajeError){
+            // Se imprime el mensaje de error de la excepción. 
+            cerr << mensajeError << endl;
+        }   
+    }
+    // Cuando todo está bien se asigna el valor de la lectura a la variable. 
+    variableEntero = int(lectura);
+}
