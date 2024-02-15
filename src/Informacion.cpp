9492c5cbc3e20aea730fa5ed3034646cc939f07e
Danny Solorzano Mayorga <dannysolorzano71@gmail.com>
2024-02-14 18:52:12 -0600

    Implementacion del metodo para generar un txt con el informe de cuotas de un prestamo con datos personalizados que se obtienen del usuario
    Signed-off-by: Danny Solorzano Mayorga <dannysolorzano71@gmail.com>

--- src/Informacion.cpp
+++ src/Informacion.cpp
@@ -64,3 +64,72 @@ void Informacion::generarInformePredeterminado(string tipo, int cuotasTotal, dou
             cout << "La opcion ingresada no es valida. Intente de nuevo." << endl;
         }
 }
+
+
+// Método para generar un informe de cuotas en archivo .txt de un préstamo con datos personalizados del usuario. 
+void Informacion::generarInformePersonalizado(){
+        // Se imprime la información del préstamo que en este caso solo es el tipo personalizado. 
+        cout << "\n---Prestamo personalizado---" << endl;
+        string tipo = "personalizado";
+        // Se lee el tipo de moneda del préstamo de parte del usuario. 
+        string tipoMoneda;
+        leerMoneda(tipoMoneda);
+        // Se lee el monto total del préstamo de parte del usuario. 
+        double montoTotal;
+        leerDouble(montoTotal, "Ingrese el monto total del prestamo: ");
+        // Se lee la cantidad total de cuotas del préstamo de parte del usuario. 
+        int cuotasTotal;
+        leerEntero(cuotasTotal, "Ingrese la cantidad total de cuotas: ");
+        // Se lee la tasa de interés del préstamo de parte del usuario. 
+        double tasaInteres;
+        leerDouble(tasaInteres, "Ingrese la tasa de interes: ");
+        // Se crea una instancia de la clase Presgamo. 
+        Prestamo prestamo;
+        // Se setean los datos con el tipo de moneda, monto total, cantidad de cuotas, tasa de interés y tipo ingresados por el usuarios. 
+        prestamo.setDatos(0,0, tipoMoneda, montoTotal, cuotasTotal, tasaInteres, 0,tipo);
+        // Se pregunta si desea generar el archivo .txt con la tabla de cuotas. 
+        cout << "Desea generar la tabla de cuotas?: " << endl;
+        cout << "1. Si." << endl;
+        cout << "2. No." << endl;
+        // Se lee la elección del usuario. 
+        int opcion;
+        leerEntero(opcion, "Ingrese una opcion: ");
+        // Si desea generar la tabla de cuotas. 
+        if(opcion == 1){
+            // Se crea y se abre un archivo .txt en modo de escritura. 
+            string nombreArchivo = "consulta_prestamo_personalizado.txt";
+            ofstream archivo(nombreArchivo, ios::out | ios::trunc);
+            // Se agrega un encabezado para el archivo. 
+            archivo << "Consulta sobre un prestamo personalizado" << endl;
+            archivo << endl;
+            // Se agrega un encabezado para la información del préstamo con los títulos de cada columna. 
+            archivo << "----------------------------Informacion del prestamo----------------------------" << endl;
+            archivo << "tipo de prestamo, tipo de moneda, monto total, cantidad cuotas, tasa interes" << endl;
+            // Se agrega la información del préstamo con los datos correspondientes a los títulos del encabezado. 
+            archivo << prestamo.tipo + "," + prestamo.tipoMoneda + ", " + to_string(prestamo.montoTotal)
+            + ", " + to_string(prestamo.cuotasTotal) + ", " + to_string(prestamo.tasaInteres) << endl;
+            // Se agrega un encabezado para la información de las cuotas con el título de cada columna. 
+            archivo << "----------------------------Cuotas a pagar en este prestamo----------------------------" << endl;
+            archivo << "numero de cuota, costo de cuota, aporte deuda, aporte intereses, monto restante" << endl;
+            // Se agrega la información de las cuotas utilizando el método de la clase Prestamo. 
+            vector <string> cuotas = prestamo.infoCuotas;
+            for (string linea : cuotas){
+                // Se agrega cada línea del vector al archivo .txt con la información de las cuotas.
+                archivo << linea << endl;
+            }
+            // Se cierra el archivo. 
+            archivo.close();
+            // Se imprime in mensaje de confirmación que indica que se logró generar el archivo sin problemas. 
+            cout << "\nSe genero el archivo: " << nombreArchivo << endl;
+        }
+        // Si no desea generar el informe de cuotas. 
+        else if(opcion == 2){
+            // Se detiene la ejecución del método. 
+            return;
+        }
+        // Si ingresa algo que no está entre las opciones dadas. 
+        else{
+            // Se imprime un mensaje de opción inválida. 
+            cout << "La opcion ingresada no es valida. Intente de nuevo." << endl;
+        }
+}
